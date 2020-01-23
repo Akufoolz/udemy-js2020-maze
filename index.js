267919892c75d6377e587c0cefc5d7ad5d277721
Foolz <akufoolz@gmail.com>
2020-01-23 16:47:54 -0700

    start of video 280

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 const { Bodies, Engine, Render, Runner, World } = Matter;
 
+const cells = 3;
+const width = 600;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: true,
+    width,
+    height
   }
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-});
-World.add(world, shape);
+// Walls
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+];
+World.add(world, walls);
+
+// Maze generation
+
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(2).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
